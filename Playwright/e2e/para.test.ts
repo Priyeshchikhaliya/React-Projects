010f5420634a7f7c2102b67ff4695b348e7e9d2e
Priyeshchikhaliya <ppriyesh61@gmail.com>
2022-07-28 14:14:42 +0530

    Done

---
+++ Playwright/e2e/para.test.ts
@@ -0,0 +1,54 @@
+import { test, expect } from "@playwright/test";
+
+test("homepage has Playwright in title and get started link linking to the intro page", async ({
+  page,
+}) => {
+  await page.goto("https://letcode.in/");
+
+  await page.locator("text=Log in").click();
+  await page.waitForURL("https://letcode.in/signin");
+  // Click text=EmailPasswordLOGIN >> [placeholder="Enter registered email"]
+  await page
+    .locator(
+      'text=EmailPasswordLOGIN >> [placeholder="Enter registered email"]'
+    )
+    .click();
+  // Fill text=EmailPasswordLOGIN >> [placeholder="Enter registered email"]
+  await page
+    .locator(
+      'text=EmailPasswordLOGIN >> [placeholder="Enter registered email"]'
+    )
+    .fill("ppriyesh61@gmail.com");
+  // Press Tab
+  await page
+    .locator(
+      'text=EmailPasswordLOGIN >> [placeholder="Enter registered email"]'
+    )
+    .press("Tab");
+  // Fill [placeholder="Enter password"]
+  await page.locator('[placeholder="Enter password"]').fill("Priyesh!9909");
+  // Click text=LOGIN
+  await page.locator("text=LOGIN").click();
+  await page.waitForURL("https://letcode.in/");
+  // Click [aria-label="Close"]
+  await page.locator('[aria-label="Close"]').click();
+  // Click [aria-label="main navigation"] >> text=Courses
+  await page.locator('[aria-label="main navigation"] >> text=Courses').click();
+  await page.waitForURL("https://letcode.in/courses");
+  // Click text=Playwright Playwright enables reliable end-to-end testing for modern web apps. I >> button
+  await page
+    .locator(
+      "text=Playwright Playwright enables reliable end-to-end testing for modern web apps. I >> button"
+    )
+    .click();
+  await page.waitForURL("https://letcode.in/playwright");
+  // Click .navbar-item >> nth=0
+  await page.locator(".navbar-item").first().click();
+  await page.waitForURL("https://letcode.in/");
+  // Click text=Sign out
+  await page.locator("text=Sign out").click();
+  await page.waitForURL("https://letcode.in/");
+  // await page.close();
+  // await context.close();
+  // await browser.close();
+});
---
+++ Playwright/tests/para.test.ts
@@ -0,0 +1,66 @@
+import { chromium, webkit, firefox } from "playwright";
+import { test, expect } from "@playwright/test";
+// describe("First Playwright", () => {
+test("This website", async () => {
+  [chromium, webkit, firefox].forEach(async (e) => {
+    const browser = await e.launch({ headless: false });
+    const context = await browser.newContext({
+      recordVideo: {
+        dir: "video/",
+      },
+    });
+    const page = await context.newPage();
+    // await page.goto("https://testingbot.com/");
+    await page.goto("https://letcode.in/");
+    // Click text=Log in
+    await page.locator("text=Log in").click();
+    await page.waitForURL("https://letcode.in/signin");
+    // Click text=EmailPasswordLOGIN >> [placeholder="Enter registered email"]
+    await page
+      .locator(
+        'text=EmailPasswordLOGIN >> [placeholder="Enter registered email"]'
+      )
+      .click();
+    // Fill text=EmailPasswordLOGIN >> [placeholder="Enter registered email"]
+    await page
+      .locator(
+        'text=EmailPasswordLOGIN >> [placeholder="Enter registered email"]'
+      )
+      .fill("ppriyesh61@gmail.com");
+    // Press Tab
+    await page
+      .locator(
+        'text=EmailPasswordLOGIN >> [placeholder="Enter registered email"]'
+      )
+      .press("Tab");
+    // Fill [placeholder="Enter password"]
+    await page.locator('[placeholder="Enter password"]').fill("Priyesh!9909");
+    // Click text=LOGIN
+    await page.locator("text=LOGIN").click();
+    await page.waitForURL("https://letcode.in/");
+    // Click [aria-label="Close"]
+    await page.locator('[aria-label="Close"]').click();
+    // Click [aria-label="main navigation"] >> text=Courses
+    await page
+      .locator('[aria-label="main navigation"] >> text=Courses')
+      .click();
+    await page.waitForURL("https://letcode.in/courses");
+    // Click text=Playwright Playwright enables reliable end-to-end testing for modern web apps. I >> button
+    await page
+      .locator(
+        "text=Playwright Playwright enables reliable end-to-end testing for modern web apps. I >> button"
+      )
+      .click();
+    await page.waitForURL("https://letcode.in/playwright");
+    // Click .navbar-item >> nth=0
+    await page.locator(".navbar-item").first().click();
+    await page.waitForURL("https://letcode.in/");
+    // Click text=Sign out
+    await page.locator("text=Sign out").click();
+    await page.waitForURL("https://letcode.in/");
+    // await page.close();
+    // await context.close();
+    // await browser.close();
+  });
+});
+// });
