ca56aadd129edf5e7b4ac8e72a14442514207d12
Priyeshchikhaliya <ppriyesh61@gmail.com>
2022-08-11 00:31:23 +0530

    Done

---
+++ React  TASK/signup-page/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Fan from './Components/Fan_Signup'
+import Talent from './Components/'
+
+export default function App() {
+  return (
+    <div>
+      <Fan/>
+      <Talent/>
+    </div>
+  );
+}
+
